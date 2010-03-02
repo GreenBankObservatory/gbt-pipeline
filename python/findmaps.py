b7483a0fab841fa9fbf392a810aeec80d35aef6a
Joe Masters <jmasters@vignesh.(none)>
2010-03-02 17:02:16 -0500

    Created faster findmaps script that uses the index file.
    The previous pyfits-based script for finding maps was too
    slow and other fits python modules have limitations.  The
    quickest and best option for speeding up the finding of maps
    is to use the already-available index file, which contains
    enough information from the equivalent fits file to be useful.

--- python/findmaps.py
+++ python/findmaps.py
@@ -1,69 +1,56 @@
 import sys
-import pyfits
-import numpy as np
+import os.path
 
-ECHO=True
+DEBUG = 1
 
-# create a temporary file in which to write mapping ranges
-rangefile = open('/tmp/kfpa-mapping-scan-ranges', 'w')
-if not rangefile:
-	print "ERROR: could not create temporary file for output."
-	sys.exit(1)
+if len(sys.argv) > 1:
+	infile = open(sys.argv[1])  # open an index file
+	if not file:
+		print "Check input file"
 
-# open the SDFITS file that may contain mapping scans
-sdfits= pyfits.open(sys.argv[1])
-if not sdfits:
-	print "ERROR: Could not open " + sys.argv[1]
-	sys.exit(1)
+RAMAP = 'RALongMap'
+DECMAP = 'DecLatMap'
 
-# create a mask with for all integrations (rows) with RALongMap as the OBSMODE
-ralongs = sdfits[1].data.field('OBSMODE').startswith("RALongMap")
+# create a temporary file for output
+outfile = open('./tmp' + os.path.basename(sys.argv[1]) + '-maps.txt', 'w')
 
-# create a mask with for all integrations (rows) with DecLatMap as the OBSMODE
-declats = sdfits[1].data.field('OBSMODE').startswith("DecLatMap")
+while 1:
 
-# combine the masks
-maprows = ralongs | declats
+  # read a line from the index file
+  line = infile.readline()
+  if not line:
+	break
+  line = line.split(' ')
+  line = [a for a in line if a.isalnum()]
+  
+  # find the start of the maps
+  if line.count(RAMAP) > 0:
+	maptype = RAMAP
+  elif line.count(DECMAP) > 0:
+	maptype = DECMAP
+  else:
+    maptype = "None"
+    continue
 
-# select all SCAN values with mapping integrations
-scans = sdfits[1].data.field('SCAN')[maprows]
+  sequence_number = int(line[7])
+  last_sequence_number = sequence_number - 1
+  lastscan = 0
 
-# make the scan list unique
-uniquescans = np.unique(scans)
+  beginscan = line[8]
+  while 1:
+	if int(line[7]) > last_sequence_number+1:
+		break
+	elif int(line[7]) < last_sequence_number:
+		break
+	last_sequence_number = int(line[7])
+	lastscan = int(line[8])
+	line = infile.readline()
+	if not line:
+		break
+	line = line.split(' ')
+	line = [a for a in line if a.isalnum()]
 
-# identify places in the list where scan numbers are non-sequential
-breaks = np.diff(uniquescans) != 1
-
-# record the number of the first mapping scan in the file
-firstmapscan = uniquescans[0]
-
-# identify the scan numbers at the beginning of each map,
-#  excluding the first mapping scan
-startscans = uniquescans[np.where((np.diff(uniquescans)!= 1)==True)[0]+1]
-
-# identify the scan number at the beginning of each map
-mapstarts = np.r_[firstmapscan,startscans]
-
-# identify the scan number at the end of each map
-mapstops  = np.r_[uniquescans[np.diff(uniquescans)!= 1],uniquescans[-1]]
-
-# combine the beginning and end mapping scans into a paired list or ranges
-mapranges = zip(mapstarts,mapstops)
-
-if ECHO:
-	print "Map scan ranges:"
-
-# write these ranges to a temporary file
-for x in range(len(mapranges)):
-	rangestr = "".join(str(mapranges[x][0]) + ',' + str(mapranges[x][1]))
-	if ECHO:
-		print rangestr
-	rangefile.write(rangestr + '\n')
-
-# close the output file
-rangefile.close()
-
-# close the SDFITS input file
-sdfits.close()
+  print  beginscan + ',' + str(lastscan)
 
 
+infile.close()
