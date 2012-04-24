d7a1ae9d7dd4ccb7261c6671cf1f60b55ee06995
Joe Masters <jmasters@nrao.edu>
2012-04-24 14:21:23 -0400

    Initial checkin of script to setup python environment.
    The following are installed in /home/gbt7/pipeline:
    
    Python-2.6.6
    numpy-1.4.1
    pyfits-2.2.2
    Obit, Revision 413
    parseltongue-2.0

---
+++ src/tools/setup-pipeline-environment.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+DATE=`date +"%s"`
+
+# ------------------------------------- create scratch and install directories
+
+SCRATCH_DIR=/home/scratch/${USER}/pipeline-install-${DATE}
+mkdir -p ${SCRATCH_DIR}
+echo created ${SCRATCH_DIR}
+
+INSTALL_DIR=/home/gbt7/pipeline
+mkdir -p ${INSTALL_DIR}
+echo created ${INSTALL_DIR}
+
+# ---------------------------- cd into scratch area and download dependencies
+
+cd ${SCRATCH_DIR}
+
+curl -O http://www.python.org/ftp/python/2.6.6/Python-2.6.6.tgz
+curl -O http://pypi.python.org/packages/source/n/numpy/numpy-1.4.1.tar.gz
+curl -O http://www.jive.nl/parseltongue/releases/parseltongue.tar.gz
+svn checkout http://svn6.assembla.com/svn/pyfits/tags/pyfits_2_2_2
+svn checkout -r 413 https://svn.cv.nrao.edu/svn/ObitInstall
+
+# alternatively...
+#curl -O http://www.jive.nl/parseltongue/releases/Obit-22JUN10a.tar.gz
+
+# ------------------------------------------------------------- build python
+
+cd ${SCRATCH_DIR}
+tar xvzf Python-2.6.6.tgz
+cd Python-2.6.6
+./configure --prefix=${INSTALL_DIR}
+make install
+
+# ------------------------------------------------------------ build numpy
+
+cd ${SCRATCH_DIR}
+tar xvzf numpy-1.4.1.tar.gz
+cd numpy-1.4.1
+ATLAS=None BLAS=None LAPACK=None ${INSTALL_DIR}/bin/python setup.py install
+
+# ------------------------------------------------------------ build pyfits
+
+cd ${SCRATCH_DIR}/pyfits_2_2_2
+${INSTALL_DIR}/bin/python setup.py install
+
+# ---------------------------------------------------------- build Obit
+
+cd ${SCRATCH_DIR}/ObitInstall
+PATH=${INSTALL_DIR}/bin:${PATH} InstallObit.sh -without ZLIB PYTHON MOTIF GLIB
+
+# ---------------------------------------------------------- build ParselTongue
+
+cd ${SCRATCH_DIR}
+tar xvzf parseltongue.tar.gz 
+cd parseltongue-2.0
+mv ./configure ./configure.sav
+
+sed 's/Obit.py/Obit.so/g' ./configure.sav > ./configure
+chmod u+x ./configure
+
+PATH=${INSTALL_DIR}/bin:${PATH} ./configure --with-obit=${SCRATCH_DIR}/ObitInstall/ObitSystem/Obit --prefix=${INSTALL_DIR}
+
+make
+make install
+
+
+exit
+
+
+# -------------------------------------------------- other useful stuff
+#
+#cd ${SCRATCH_DIR}
+#curl -O http://pypi.python.org/packages/2.6/s/setuptools/setuptools-0.6c11-py2.6.egg
+#
+#PATH=${INSTALL_DIR}/bin:${PATH} sh setuptools-0.6c11-py2.6.egg --prefix=${INSTALL_DIR}
+#${INSTALL_DIR}/bin/easy_install pip
+#${INSTALL_DIR}/bin/pip install matplotlib
+#
+## edit matplotlib source to get it to work
+#sed s/numpy.ma/numpy.core.ma/ ${INSTALL_DIR}/lib/python2.6/site-packages/matplotlib/numerix/npyma/__init__.py >tempfile
+#mv tempfile ${INSTALL_DIR}/lib/python2.6/site-packages/matplotlib/numerix/npyma/__init__.py
+#sed s/numpy.ma/numpy.core.ma/ ${INSTALL_DIR}/lib/python2.6/site-packages/matplotlib/numerix/ma/__init__.py >tempfile
+#mv tempfile ${INSTALL_DIR}/lib/python2.6/site-packages/matplotlib/numerix/ma/__init__.py
+#
+#${INSTALL_DIR}/bin/pip install ipython
+#
+## --------------------------------------------- remove scratch build area
+#
+#rm -rf ${SCRATCH_DIR}
