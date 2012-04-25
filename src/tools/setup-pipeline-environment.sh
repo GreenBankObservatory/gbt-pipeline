12d47d71dcc0d51bb0fc2f6f0bea6f549ff3e648
Joe Masters <jmasters@nrao.edu>
2012-04-25 16:15:36 -0400

    Added some echo statements to pipeline environment install script.

--- src/tools/setup-pipeline-environment.sh
+++ src/tools/setup-pipeline-environment.sh
@@ -15,18 +15,22 @@ echo created ${INSTALL_DIR}
 # ---------------------------- cd into scratch area and download dependencies
 
 cd ${SCRATCH_DIR}
-
+echo 'downloading python-2.6.6'
 curl -O http://www.python.org/ftp/python/2.6.6/Python-2.6.6.tgz
+echo 'downloading numpy-1.4.1'
 curl -O http://pypi.python.org/packages/source/n/numpy/numpy-1.4.1.tar.gz
+echo 'downloading parseltongue-2.0'
 curl -O http://www.jive.nl/parseltongue/releases/parseltongue.tar.gz
+echo 'downloading pyfits-2.2.2'
 svn checkout http://svn6.assembla.com/svn/pyfits/tags/pyfits_2_2_2
+echo 'downloading Obit Revision 413'
 svn checkout -r 413 https://svn.cv.nrao.edu/svn/ObitInstall
 
 # alternatively...
 #curl -O http://www.jive.nl/parseltongue/releases/Obit-22JUN10a.tar.gz
 
 # ------------------------------------------------------------- build python
-
+echo 'building python'
 cd ${SCRATCH_DIR}
 tar xvzf Python-2.6.6.tgz
 cd Python-2.6.6
@@ -34,29 +38,29 @@ cd Python-2.6.6
 make install
 
 # ------------------------------------------------------------ build numpy
-
+echo 'building numpy'
 cd ${SCRATCH_DIR}
 tar xvzf numpy-1.4.1.tar.gz
 cd numpy-1.4.1
 ATLAS=None BLAS=None LAPACK=None ${INSTALL_DIR}/bin/python setup.py install
 
 # ------------------------------------------------------------ build pyfits
-
+echo 'building pyfits'
 cd ${SCRATCH_DIR}/pyfits_2_2_2
 ${INSTALL_DIR}/bin/python setup.py install
 
 # ---------------------------------------------------------- build Obit
-
+echo 'building Obit'
 cd ${SCRATCH_DIR}/ObitInstall
 PATH=${INSTALL_DIR}/bin:${PATH} InstallObit.sh -without ZLIB PYTHON MOTIF GLIB
 
 # ---------------------------------------------------------- build ParselTongue
-
+echo 'building ParselTongue'
 cd ${SCRATCH_DIR}
 tar xvzf parseltongue.tar.gz 
 cd parseltongue-2.0
 mv ./configure ./configure.sav
-
+echo 'updating ParselTongue configure script'
 sed 's/Obit.py/Obit.so/g' ./configure.sav > ./configure
 chmod u+x ./configure
 
