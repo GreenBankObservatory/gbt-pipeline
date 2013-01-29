a2812085f742977ef2de3e71892c60a086a04970
Joe Masters <jmasters@nrao.edu>
2013-01-29 14:34:56 -0500

    Added new values to header of plot.

--- src/spectralpipe/show_reduced.py
+++ src/spectralpipe/show_reduced.py
@@ -2,7 +2,6 @@ import numpy as np
 import fitsio
 import sys
 import pylab as pl
-from scipy import constants
 import os
 
 def freq_axis(data,verbose=0):
@@ -26,6 +25,37 @@ def freq_axis(data,verbose=0):
 
     return faxis
 
+# from http://www.bdnyc.org/2012/10/15/decimal-deg-to-hms/
+# convert decimal degrees to HMS format
+def deg2HMS(ra='', dec='', round=False):
+  RA, DEC, rs, ds = '', '', '', ''
+  if dec:
+    if str(dec)[0] == '-':
+      ds, dec = '-', abs(dec)
+    deg = int(dec)
+    decM = abs(int((dec-deg)*60))
+    if round:
+      decS = int((abs((dec-deg)*60)-decM)*60)
+    else:
+      decS = (abs((dec-deg)*60)-decM)*60
+    DEC = '{0}{1} {2} {3}'.format(ds, deg, decM, decS)
+  
+  if ra:
+    if str(ra)[0] == '-':
+      rs, ra = '-', abs(ra)
+    raH = int(ra/15)
+    raM = int(((ra/15)-raH)*60)
+    if round:
+      raS = int(((((ra/15)-raH)*60)-raM)*60)
+    else:
+      raS = ((((ra/15)-raH)*60)-raM)*60
+    RA = '{0}{1} {2} {3}'.format(rs, raH, raM, raS)
+  
+  if ra and dec:
+    return (RA, DEC)
+  else:
+    return RA or DEC
+
 # Convert frequency to velocity (m/s) using the given rest
 # frequency and velocity definition.  The units (Hz, MHz, GHz, etc)
 # of the frequencies to convert must match that of the rest frequency 
@@ -41,7 +71,7 @@ def freq_axis(data,verbose=0):
 # @returns velocity in m/s
 def freqtovel(freq, restfreq, veldef='RADIO'):
 
-    LIGHT_SPEED = constants.c/1e3 # km/s
+    LIGHT_SPEED = 299792458.0/1e3 # speed of light in a vacuum (km/s)
     freq = float(freq)
     restfreq = float(restfreq)
     
@@ -60,7 +90,10 @@ def freqtovel(freq, restfreq, veldef='RADIO'):
 
 if __name__ == '__main__':
 
-    ff = fitsio.FITS(sys.argv[1])
+    infile = sys.argv[1]
+
+    ff = fitsio.FITS(infile)
+    hdr = fitsio.read_header(infile, 1)
 
     fignum = 0
     for ext in range(len(ff)):
@@ -69,30 +102,47 @@ if __name__ == '__main__':
 	    for row in range(len(tdata)):
 		if not np.all(np.isnan(tdata['DATA'][row])):
 		    target_name = tdata['OBJECT'][row].strip()
-		    bandwidth = tdata['BANDWID'][row]/1e6
-		    procname = tdata['OBSMODE'][row].strip().split(':')[0]
+                    projid = tdata['PROJID'][row].strip()
 		    coord1name = tdata['CTYPE2'][row].strip()
 		    coord1val = tdata['CRVAL2'][row]
 		    coord2name = tdata['CTYPE3'][row].strip()
 		    coord2val = tdata['CRVAL3'][row]
+                    cv1,cv2 = deg2HMS(ra=coord1val, dec=coord2val, round=True)
 		    fsky = tdata['CRVAL1'][row]/1e9
 		    azimuth = tdata['AZIMUTH'][row]
 		    elevation = tdata['ELEVATIO'][row]
 		    lst = tdata['LST'][row]/1e9
 		    tsys = tdata['TSYS'][row]
-		    date = tdata['DATE-OBS'][row]
+		    date = tdata['TIMESTAMP'][row]
+                    restfreq = tdata['RESTFREQ'][row]/1e9
+                    velocity = int(tdata['VELOCITY'][row]/1e3)
+                    velframe = tdata['VELDEF'][row]
+
+                    exposure = tdata['EXPOSURE'][row]
+                    integ_h = int(exposure/3600.)
+                    exposure -=  integ_h*3600
+                    integ_m = int(exposure/60)
+                    integ_s = exposure-integ_m*60
 		    
+                    scans = hdr['SCANLIST']
+
 		    titlestring = (
-		    'Bandwidth: {bw:.2f} MHz\n'
-		    'Procedure: {pn}\n'
+                    'SCANS  {scans}\n'
+                    'Project ID {pid}\n'
 		    'Sky Frequency {fs:.3f} GHz\n'
+                    'Rest Frequency {rf:.3f} GHz\n'
+                    'Velocity {vel:d} km/s\n'
+                    'Velocity reference frame {vframe}\n'
 		    'Date {date}\n'
-		    '{cn1}:{cv1:.1f}  {cn2}:{cv2:.1f}\n'
+		    '{cn1}:{cv1}  {cn2}:{cv2}\n'
 		    'AZ: {az:.1f}   EL: {el:.1f}\n'
+                    'Integration time {hh:d} {mm:d} {ss:.1f}\n'
 		    'Tsys {tsys:.2f}').format(
-			bw=bandwidth, pn=procname, fs=fsky, cn1=coord1name,
-			cv1=coord1val, cn2=coord2name, cv2=coord2val,
-			az=azimuth, el=elevation, tsys=tsys, date=date)
+			pid=projid, fs=fsky, rf=restfreq, cn1=coord1name,
+			cv1=cv1, cn2=coord2name, cv2=cv2, vel=velocity,
+                        vframe=velframe, hh=integ_h, mm=integ_m, ss=integ_s,
+			az=azimuth, el=elevation, tsys=tsys, date=date,
+                        ex=exposure, scans=scans)
 		    
 		    fig = pl.figure(fignum)
 
