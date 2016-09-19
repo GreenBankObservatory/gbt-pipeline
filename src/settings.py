e802be3813df865b1962916526f06978ed8bbb06
Joe Masters <jmasters@nrao.edu>
2016-09-19 13:29:58 -0400

    Updated pipeline version to 1.3 for next release.  Updated CHANGES.

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,3 @@
 CREATE_PLOTS = False
-PIPELINE_VERSION = '1.2.1-dev'  # to record in output primary header
+PIPELINE_VERSION = '1.3'  # to record in output primary header
 PARALLEL = True  # useful to turn off when debugging
