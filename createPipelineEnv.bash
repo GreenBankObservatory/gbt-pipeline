61b44c841d591c83edef6f73aad40e4ae9027524
Kathlyn Purcell <kpurcell@belinda.gb.nrao.edu>
2023-03-09 16:10:55 -0500

    Changing the python env to work on all users, also updaing the examples to be a relative path

--- createPipelineEnv.bash
+++ createPipelineEnv.bash
@@ -13,10 +13,11 @@ fi
 export LD_LIBRARY_PATH=/opt/local/lib:$LD_LIBRARY_PATH
 #virtualenv --python=/home/gbt7/newt/bin/python "$venv_path"
 # If you're going to make a new venv, you should use 2.7.18. I stuck that in a venv for monctrl that you could use:
-/home/sandboxes/monctrl/venvs/monctrl-2.7.18-env/bin/virtualenv --always-copy "$venv_path"
+virtualenv -p python2.7 "$venv_path"
 
 # Enter the virtual environment
 source "$venv_path/bin/activate"
+pip install --upgrade pip
 # Install numpy prior to other requirements due to poor dependency resolution in early pip versions
 pip install numpy==1.6.2
 # Install the rest of the requirements
